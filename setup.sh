c36fe52910047b158bd19deb306b7cad5a43b909
Keir Finlow-Bates <keirfb@gmail.com>
2023-12-17 01:48:46 +0200

    Has reordering made it work?

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # clone repositories in build folder
+mkdir -p build
 cd ./build
 git clone https://github.com/micropython/micropython.git
 git clone https://github.com/russhughes/s3lcd.git
@@ -22,19 +23,19 @@ cd ..
 cd s3lcd/
 git checkout 1946749dc68789240480834083f3d779c1415f37
 
-# build correct version of micropython
+# check out and edit version of micropython
 cd ..
 cd micropython/
 git checkout v1.20.0
 git submodule update --init
-cd mpy-cross/
-make
-cd ..
-cd ports/esp32
 
 # update the board definition for the T-Display-3 which has a 16MB octal SPIRAM 
+cd ports/esp32
 sed -i 's/8MB=y/8MB=/' ./boards/GENERIC_S3_SPIRAM_OCT/sdkconfig.board
 sed -i 's/16MB=/16MB=y/' ./boards/GENERIC_S3_SPIRAM_OCT/sdkconfig.board
 # sed -i 's/8MiB/16MiB/' ./boards/GENERIC_S3_SPIRAM_OCT/sdkconfig.board
+
+cd ../../mpy-cross/
+make
 
 # build.sh should now build the firmware
